a6aceb58e20a81c38c25a00a89d74ea9ffb9e2fc
Akmal Rafi Diara Putra <49126626+akmalrafidiara@users.noreply.github.com>
2023-12-25 00:21:03 +0700

    Update React and React DOM versions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, render } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
